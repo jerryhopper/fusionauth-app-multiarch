abfcb1e7a0ad78aff67d75d9843e1e9660c2d0ba
JerryHopper <hopper.jerry@gmail.com>
2020-11-03 10:27:11 +0100

    Update buildx.sh
    version update 1.20.1

--- extra/buildx.sh
+++ extra/buildx.sh
@@ -3,4 +3,4 @@
 # Buildscript for buildx
 # this script requires a proper buildx enviroment.
 
-docker buildx build --force-rm --platform linux/arm/v7,linux/arm64,linux/amd64 -t jerryhopper/fusionauth-app:latest https://github.com/jerryhopper/fusionauth-app-multiarch.git
+docker buildx build --force-rm --platform linux/arm/v7,linux/arm64,linux/amd64 -t jerryhopper/fusionauth-app:1.20.1 https://github.com/jerryhopper/fusionauth-app-multiarch.git
